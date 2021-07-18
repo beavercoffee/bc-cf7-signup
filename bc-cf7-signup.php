5166bf84159f5d4e43e5d01da86afb2144cf294c
beavercoffee <beavercoffee@vidsoe.com>
2021-07-18 17:16:17 -0600

    BC

---
+++ bc-cf7-signup.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Author: Beaver Coffee
+Author URI: https://beaver.coffee
+Description: Sign up with Contact Form 7.
+Domain Path:
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Network: true
+Plugin Name: BC CF7 Signup
+Plugin URI: https://github.com/beavercoffee/bc-cf7-signup
+Requires at least: 5.7
+Requires PHP: 5.6
+Text Domain: bc-cf7-signup
+Version: 1.7.18
+*/
+
+if(defined('ABSPATH')){
+    require_once(plugin_dir_path(__FILE__) . 'classes/class-bc-cf7-signup.php');
+    BC_CF7_Signup::get_instance(__FILE__);
+}
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== BC CF7 Signup ===
+Contributors: beavercoffee
+Donate link: https://beaver.coffee
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Requires at least: 5.7
+Requires PHP: 5.6
+Stable tag: 1.7.18
+Tags:
+Tested up to: 5.7.2
+
+BC CF7 Signup
+
+== Description ==
+
+Sign up with Contact Form 7.
+
+== Changelog ==
+
+[Changelog »](https://github.com/beavercoffee/bc-cf7-signup "BC CF7 Signup")
